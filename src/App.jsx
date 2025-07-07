2307b5fa3a1cf7ec4538d7cbeab33fb5afb45aa8
heathbo1 <heathbo1@hotmail.com>
2025-07-07 09:21:10 -0500

    Home modal update

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,9 @@ const App = () => {
 
               <li>It was built using TypeScript, React, SCSS, Bootstrap, Prettier, React Router, ESLint, Node.js, and Vite.</li>
               <ul>
-                <li>Bootstrap was used in the Header (for horizontal resizing) and in the Modals (modal functionality). Everything else is custom. - You'll find my initials HMB at the start of every custom CSS class name as proof.</li>
+                <li>
+                  Bootstrap was used in the Header (for horizontal resizing) and in the Modals (basic modal functionality). Everything else is custom. - You'll find my initials HMB at the start of every custom CSS class name as proof.
+                </li>
               </ul>
               <li>The background stars are randomly generated, and the comets have random start and stop locations, colors, velocities, and sizes.</li>
             </ul>
